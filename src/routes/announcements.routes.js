c8ac715e9c361308267e88e70aee5c0a830adc94
Max Kosobutskyi <maxkosobutskyi@gmail.com>
2026-06-16 06:26:42 +0300

    Define routes, connect router

--- src/routes/announcements.routes.js
+++ src/routes/announcements.routes.js
@@ -1,2 +1,37 @@
 import express from "express";
 import * as announcementsController from "../controllers/announcements.controller.js";
+import * as announcementsValidator from "../validators/announcements.validator.js";
+
+const router = express.Router();
+
+router.get(
+  "/",
+  announcementsValidator.getAnnouncementsValidator,
+  announcementsController.getAllAnnouncements,
+);
+
+router.get(
+  "/:id",
+  announcementsValidator.getAnnouncementValidator,
+  announcementsController.getAnnouncementById,
+);
+
+router.post(
+  "/",
+  announcementsValidator.createAnnouncementValidator,
+  announcementsController.createAnnouncement,
+);
+
+router.patch(
+  "/:id",
+  announcementsValidator.updateAnnouncementValidator,
+  announcementsController.updateAnnouncement,
+);
+
+router.delete(
+  "/:id",
+  announcementsValidator.deleteAnnouncementValidator,
+  announcementsController.deleteAnnouncement,
+);
+
+export default router;
